b0ed870156c9b99cc6a5ff31ba731188e07330fd
Angelica Rees <angelkdrees@gmail.com>
2023-02-28 12:57:15 -0500

    Basic Structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import NotesList from './components/NotesList';
+import {useState} from 'react';
+import {nanoid} from 'nanoid';
 
 const App = () => {
+  const [notes, setNotes] = useState([
+  {
+    id: nanoid(),
+    text: "This is my first note!",
+    date: "2/28/2023"
+  },
+  {
+    id: nanoid(),
+    text: "This is my second note!",
+    date: "3/02/2023"
+  },
+  {
+    id: nanoid(),
+    text: "This is my third note!",
+    date: "7/01/2023"
+  }]);
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes = {notes}/>
     </div>
   );
 };
